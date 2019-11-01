fbe1a2024aed7fb00a3f7c7e4e8be34a76a4a112
Ariel Rossanigo <arielrossanigo@gmail.com>
2019-11-01 17:56:51 -0300

    first commit

---
+++ generate_dataset.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+
+
+np.random.seed(42)
+
+
+class_distribution = pd.read_csv('class_distribution.csv')
+class_distribution = class_distribution.set_index('race')
+
+race_distribution = pd.read_csv('race_distribution.csv')
+race_distribution = race_distribution.set_index('race').iloc[:, 0]
+
+races = {
+    'dwarf': {'height': ((4, 2), (4, 8)), 'weight': (134, 226), 'age': (300, 50), 'strength': 0, 'charisma': 0, 'constitution': 2},
+    'gnome': {'height': ((3, 1), (3, 7)), 'weight': (37, 43), 'age': (425, 75), 'strength': 0, 'charisma': 0, 'constitution': 0},
+    'elf': {'height': ((4, 8), (6, 2)), 'weight': (92, 170), 'age': (700, 50), 'strength': 0, 'charisma': 0, 'constitution': 0},
+    'halfling': {'height': ((2, 9), (3, 3)), 'weight': (33, 38), 'age': (150, 50), 'strength': 0, 'charisma': 0, 'constitution': 0},
+    'half-elf': {'height': ((4, 11), (6, 1)), 'weight': (114, 238), 'age': (150, 10), 'strength': 1, 'charisma': 2, 'constitution': 1},
+    'human': {'height': ((4, 9), (5, 7)), 'weight': (114, 270), 'age': (70, 5), 'strength': 1, 'charisma': 1, 'constitution': 1},
+    'dragonborn': {'height': ((5, 8), (6, 1)), 'weight': (179, 367), 'age': (60, 10), 'strength': 2, 'charisma': 1, 'constitution': 0},
+    'half-orc': {'height': ((5, 0), (6, 6)), 'weight': (144, 380), 'age': (70, 5), 'strength': 2, 'charisma': 0, 'constitution': 1},
+    'tiefling': {'height': ((4, 11), (6, 1)), 'weight': (114, 238), 'age': (80, 10), 'strength': 0, 'charisma': 2, 'constitution': 0},
+}
+
+
+N = 100_000
+characters = []
+
+for race in races:
+    n = int(N * race_distribution.loc[race])
+    low_height, high_height = races[race]['height']
+    low_height = low_height[0] * 13 + low_height[1]
+    high_height = high_height[0] * 13 + high_height[1]
+    d = (high_height - low_height) / 2
+    u = d + low_height
+    heights = np.random.normal(u, d*.3, size=n)
+
+    low_weight, high_weight = races[race]['weight']
+    d = (high_weight - low_weight) / 2
+    u = d + low_weight
+    weights = np.random.normal(u, d*.3, size=n)
+
+    age_u, age_v = races[race]['age']
+    ages = np.random.normal(age_u, age_v*.3, size=n)
+
+    classes = class_distribution.loc[race]
+    class_ = classes.sample(n, weights=classes, replace=True).index
+
+    df = pd.DataFrame({
+        'height': heights,
+        'weight': weights,
+        'age': ages,
+        'class': class_,
+        'race': [race] * n,
+        'strength': [races[race]['strength']] * n,
+        'charisma': [races[race]['charisma']] * n,
+        'constitution': [races[race]['constitution']] * n,
+    })
+    characters.append(df)
+
+all_data = pd.concat(characters)
+
+all_data.to_csv('characters.csv', index=False)
---
+++ under_the_carpet.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy as sp
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import cross_val_predict, StratifiedKFold
+from sklearn.datasets import make_classification
+from sklearn import cluster, datasets
+from tqdm import tqdm
+from umap import UMAP
+from pynndescent import NNDescent
+from fastcluster import single
+from scipy.cluster.hierarchy import cut_tree, fcluster, dendrogram
+from scipy.spatial.distance import squareform
+from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
+from sklearn import metrics
+from sklearn_pandas.dataframe_mapper import DataFrameMapper
+from sklearn.decomposition import PCA
+import random
+from sklearn.preprocessing import LabelEncoder
+
+import warnings
+warnings.filterwarnings('ignore')
+sns.set_style('white')
+
+
+def plot_data(data, labels=None, title=None, legend=True, centroids=None):
+    plt.figure(figsize=(7,5), dpi=100)
+    if labels is not None:
+        if isinstance(labels, list):
+            labels = np.array(labels)
+
+        unique_labels = list(sorted(set(x for x in labels if str(x) != '-1')))
+        labels = np.array(list(map(str, labels)))
+        if '-1' in labels:
+            plt.scatter(data[labels=='-1', 0], data[labels=='-1', 1], s=5, c='black', alpha=0.5)
+
+
+        for i, label in enumerate(unique_labels):
+            label = str(label)
+            plt.scatter(data[labels==label, 0], data[labels==label, 1], s=5, c='C{}'.format(i),
+                        cmap='viridis', label=label, alpha=0.5)
+
+            if centroids is not None:
+                plt.scatter(centroids[i, 0], centroids[i, 1], s=250, c='white', marker='o')
+                plt.scatter(centroids[i, 0], centroids[i, 1], s=100, marker='X')
+
+        if legend:
+            plt.legend(fontsize=10, markerscale=3, loc='upper left', bbox_to_anchor=(1, 1))
+    else:
+        plt.scatter(data[:, 0], data[:, 1], s=5, color='grey', alpha=0.5)
+
+    if title:
+        plt.title(title)
+    plt.axis('off')
+
+
+def load_mnist(size):
+    X, y = datasets.fetch_openml('mnist_784', version=1, return_X_y=True)
+    sample_index = np.random.choice(list(range(len(X))), size=size, replace=False)
+    digits_X = X[sample_index]
+    digits_y = y[sample_index]
+    return digits_X, digits_y
+
+
+def load_dd(size):
+    characters = pd.read_csv('characters.csv')
+    characters = characters.sample(size, random_state=42)
+    mapper = {
+        'dragonborn': 0,
+        'dwarf': 0,
+        'elf': 1,
+        'gnome': 0,
+        'half-elf': 1,
+        'half-orc': 0,
+        'halfling': 0,
+        'human': 1,
+        'tiefling': 0
+    }
+    characters['most_common_race'] = characters.race.map(mapper)
+    return characters
+
